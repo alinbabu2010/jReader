0e8570663c5d8a2bd6131aef50d5b8efdb8d54fb
Alin Babu <alinbabu2010@gmail.com>
2022-05-18 14:07:28 +0530

    Add ReaderScreens enum constants and app routing

--- app/src/main/java/com/compose/jreader/ui/navigation/ReaderScreens.kt
+++ app/src/main/java/com/compose/jreader/ui/navigation/ReaderScreens.kt
@@ -1,4 +1,29 @@
 package com.compose.jreader.ui.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    HomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    StatusScreen;
+
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens =
+            when (route?.substringBefore("/")) {
+                SplashScreen.name -> SplashScreen
+                LoginScreen.name -> LoginScreen
+                CreateAccountScreen.name -> CreateAccountScreen
+                HomeScreen.name -> HomeScreen
+                SearchScreen.name -> SearchScreen
+                DetailScreen.name -> DetailScreen
+                UpdateScreen.name -> UpdateScreen
+                StatusScreen.name -> StatusScreen
+                null -> HomeScreen
+                else -> throw IllegalArgumentException("Route $route is not recognised")
+            }
+    }
+
 }
