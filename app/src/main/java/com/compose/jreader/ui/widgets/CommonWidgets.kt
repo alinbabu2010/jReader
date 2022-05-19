f4288cbee363d5ce2aab8e41f83735a7a58064f4
Alin Babu <alinbabu2010@gmail.com>
2022-05-19 14:50:43 +0530

    Add composable for input forms

--- app/src/main/java/com/compose/jreader/ui/widgets/CommonWidgets.kt
+++ app/src/main/java/com/compose/jreader/ui/widgets/CommonWidgets.kt
@@ -1,13 +1,24 @@
 package com.compose.jreader.ui.widgets
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import com.compose.jreader.R
+import com.compose.jreader.utils.inputFontSize
+import com.compose.jreader.utils.inputPadding
 import com.compose.jreader.utils.logoTextBottomPadding
 
 
@@ -19,4 +30,50 @@ fun ReaderLogo(modifier: Modifier = Modifier) {
         style = MaterialTheme.typography.h3,
         color = Color.Red.copy(alpha = 0.5f)
     )
+}
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    label: String,
+    isSingleLine: Boolean = true,
+    enabled: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardAction: KeyboardActions = KeyboardActions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    trailingIcon: @Composable () -> Unit = {}
+) {
+
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+            valueState.value = it
+        },
+        label = {
+            Text(text = label)
+        },
+        singleLine = isSingleLine,
+        enabled = enabled,
+        textStyle = TextStyle(
+            fontSize = inputFontSize,
+            color = MaterialTheme.colors.onBackground
+        ),
+        modifier = modifier
+            .padding(
+                bottom = inputPadding,
+                start = inputPadding,
+                end = inputPadding
+            )
+            .fillMaxWidth(),
+        keyboardActions = keyboardAction,
+        keyboardOptions = KeyboardOptions(
+            imeAction = imeAction,
+            keyboardType = keyboardType
+        ),
+        visualTransformation = visualTransformation,
+        trailingIcon = trailingIcon
+    )
+
 }
---
+++ app/src/main/java/com/compose/jreader/ui/widgets/FormWidgets.kt
@@ -0,0 +1,68 @@
+package com.compose.jreader.ui.widgets
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.compose.jreader.R
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onKeyboardAction: KeyboardActions = KeyboardActions.Default
+) {
+    InputField(
+        modifier = modifier,
+        valueState = emailState,
+        label = stringResource(R.string.email),
+        enabled = enabled,
+        imeAction = imeAction,
+        keyboardAction = onKeyboardAction
+    )
+}
+
+@Composable
+fun PasswordInput(
+    modifier: Modifier = Modifier,
+    passwordState: MutableState<String>,
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    passwordVisibility: MutableState<Boolean>,
+    onKeyboardAction: KeyboardActions = KeyboardActions.Default
+) {
+
+    val visualTransformation = if (passwordVisibility.value) VisualTransformation.None
+    else PasswordVisualTransformation()
+
+    InputField(
+        modifier = modifier,
+        valueState = passwordState,
+        label = stringResource(R.string.password),
+        enabled = enabled,
+        imeAction = imeAction,
+        keyboardAction = onKeyboardAction,
+        keyboardType = KeyboardType.Password,
+        visualTransformation = visualTransformation
+    ) {
+        PasswordVisibility(passwordVisibility = passwordVisibility)
+    }
+}
+
+@Composable
+fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
+    val visible = passwordVisibility.value
+    IconButton(onClick = { passwordVisibility.value = !visible }) {
+        Icons.Default.Close
+    }
+}
