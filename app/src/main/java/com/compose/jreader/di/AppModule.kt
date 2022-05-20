392b21d69e6c741c3b44b41b26c9f55efb95d9a3
Alin Babu <alinbabu2010@gmail.com>
2022-05-20 15:33:33 +0530

    Add dependency providers for firebase instances

--- app/src/main/java/com/compose/jreader/di/AppModule.kt
+++ app/src/main/java/com/compose/jreader/di/AppModule.kt
@@ -1,12 +1,23 @@
 package com.compose.jreader.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFireStore() = FirebaseFirestore.getInstance()
 
 }
