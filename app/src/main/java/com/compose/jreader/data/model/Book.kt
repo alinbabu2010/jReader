49b8ce2c02ebbdb2b279de911ad78a13f2ba8f92
Alin Babu <alinbabu2010@gmail.com>
2022-05-23 16:50:22 +0530

    Modify model class and add dummy data

--- app/src/main/java/com/compose/jreader/data/model/Book.kt
+++ app/src/main/java/com/compose/jreader/data/model/Book.kt
@@ -3,6 +3,44 @@ package com.compose.jreader.data.model
 data class Book(
     var id: String? = null,
     var title: String? = null,
+    var photoUrl: String? = null,
     var authors: String? = null,
     var notes: String? = null
-)
+) {
+
+    companion object {
+
+        fun getBooks(): List<Book> = listOf(
+            Book(
+                "12",
+                "Running",
+                "http://books.google.com/books/content?id=Wu5qDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                "Jacob",
+                "Good, interesting one"
+            ),
+            Book(
+                "12",
+                "Running",
+                "http://books.google.com/books/content?id=Wu5qDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                "Jacob",
+                "Good, interesting one"
+            ),
+            Book(
+                "12",
+                "Running",
+                "http://books.google.com/books/content?id=Wu5qDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                "Jacob",
+                "Good, interesting one"
+            ),
+            Book(
+                "12",
+                "Running",
+                "http://books.google.com/books/content?id=Wu5qDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                "Jacob",
+                "Good, interesting one"
+            )
+        )
+
+    }
+
+}
