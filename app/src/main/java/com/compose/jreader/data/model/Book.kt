2eddeda46de03e4faa9b27af8fb2578ca9244055
Alin Babu <alinbabu2010@gmail.com>
2022-05-23 10:48:34 +0530

    Add model class for book

---
+++ app/src/main/java/com/compose/jreader/data/model/Book.kt
@@ -0,0 +1,8 @@
+package com.compose.jreader.data.model
+
+data class Book(
+    var id: String? = null,
+    var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null
+)
