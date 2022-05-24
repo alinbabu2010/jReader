05c2f3b6ccdfcf034c5867862c9136cf4032d04a
Alin Babu <alinbabu2010@gmail.com>
2022-05-24 10:30:37 +0530

    Add topBar UI to search screen

--- app/src/main/java/com/compose/jreader/ui/screens/search/ReaderBookSearchScreen.kt
+++ app/src/main/java/com/compose/jreader/ui/screens/search/ReaderBookSearchScreen.kt
@@ -1,9 +1,31 @@
 package com.compose.jreader.ui.screens.search
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.compose.jreader.ui.components.ReaderAppBar
 
+@Preview
 @Composable
-fun ReaderBookSearchScreen(navController: NavHostController) {
+fun ReaderBookSearchScreen(navController: NavHostController = NavHostController(LocalContext.current)) {
+
+    Scaffold(topBar = {
+        ReaderAppBar(
+            title = "Search Books",
+            navController = navController,
+            icon = Icons.Default.ArrowBack,
+            showProfile = false
+        ) {
+            navController.popBackStack()
+        }
+
+    }) {
+
+
+    }
 
 }
