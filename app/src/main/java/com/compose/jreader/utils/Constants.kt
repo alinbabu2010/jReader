9fd90229865d8f7e3c6b6d4311bd902a1c260147
Alin Babu <alinbabu2010@gmail.com>
2022-05-25 13:57:27 +0530

    Add constant values

---
+++ app/src/main/java/com/compose/jreader/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.compose.jreader.utils
+
+object Constants {
+
+    const val ENDPOINT_VOLUME = "volumes"
+    const val ENDPOINT_BOOK_INFO = "volumes/{bookId}"
+
+    const val PARAM_QUERY = "q"
+    const val PARAM_BOOKID = "bookId"
+
+    const val NETWORK_FAILURE = "Network error occurred. Please try later"
+
+}
