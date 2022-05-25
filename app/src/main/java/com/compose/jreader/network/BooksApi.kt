db7354a12c617026b9d00d0c940aa6d52375f2bb
Alin Babu <alinbabu2010@gmail.com>
2022-05-25 14:17:07 +0530

    Add retrofit interface function

---
+++ app/src/main/java/com/compose/jreader/network/BooksApi.kt
@@ -0,0 +1,25 @@
+package com.compose.jreader.network
+
+import com.compose.jreader.data.model.Book
+import com.compose.jreader.data.model.BookList
+import com.compose.jreader.utils.Constants
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BooksApi {
+
+    @GET(Constants.ENDPOINT_VOLUME)
+    fun getAllBooks(
+        @Query(Constants.PARAM_QUERY) query: String
+    ): Call<BookList>
+
+    @GET(Constants.ENDPOINT_BOOK_INFO)
+    fun getBookInfo(
+        @Path(Constants.PARAM_BOOKID) bookId: String
+    ): Call<Book>
+
+}
