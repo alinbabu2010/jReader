c3979fe0b644e6c5a849b29273c041594f331cff
Alin Babu <alinbabu2010@gmail.com>
2022-05-25 14:16:42 +0530

    Add retrofit setup in hilt-dagger module

--- app/src/main/java/com/compose/jreader/di/AppModule.kt
+++ app/src/main/java/com/compose/jreader/di/AppModule.kt
@@ -1,11 +1,17 @@
 package com.compose.jreader.di
 
+import com.compose.jreader.BuildConfig
+import com.compose.jreader.network.BooksApi
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -19,5 +25,31 @@ object AppModule {
     @Singleton
     @Provides
     fun provideFireStore() = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val logLevel = if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        return HttpLoggingInterceptor().setLevel(logLevel)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideBookApi(httpClient: OkHttpClient): BooksApi {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(BooksApi::class.java)
+    }
 
 }
