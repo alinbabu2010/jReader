aedb9a316e91318313093de4d6eb0aeceefd7335
Alin Babu <alinbabu2010@gmail.com>
2022-05-26 10:59:04 +0530

    Modify search view composable and navigation to details screen

--- app/src/main/java/com/compose/jreader/ui/screens/search/ReaderBookSearchScreen.kt
+++ app/src/main/java/com/compose/jreader/ui/screens/search/ReaderBookSearchScreen.kt
@@ -1,10 +1,9 @@
 package com.compose.jreader.ui.screens.search
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
@@ -13,7 +12,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -21,18 +19,18 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.compose.jreader.R
 import com.compose.jreader.data.model.BookUi
-import com.compose.jreader.data.model.UiState
+import com.compose.jreader.ui.components.FadeVisibility
 import com.compose.jreader.ui.components.InputField
+import com.compose.jreader.ui.components.LoaderMessageView
 import com.compose.jreader.ui.components.ReaderAppBar
+import com.compose.jreader.ui.navigation.ReaderScreens
 import com.compose.jreader.utils.*
 
 
@@ -73,8 +71,8 @@ fun ReaderBookSearchScreen(
                     viewModel.searchBooks(query)
                     uiState = viewModel.listOfBooks.value
                 }
-                BookListView(!uiState.isLoading, uiState.data ?: emptyList())
-                ContainerView(uiState)
+                BookListView(!uiState.isLoading, uiState.data, navController)
+                LoaderMessageView(uiState)
             }
         }
 
@@ -85,59 +83,41 @@ fun ReaderBookSearchScreen(
 @Composable
 fun BookListView(
     isVisible: Boolean,
-    books: List<BookUi>
+    books: List<BookUi>?,
+    navController: NavHostController
 ) {
-    AnimatedVisibility(
-        visible = isVisible,
-        enter = fadeIn(),
-        exit = fadeOut()
-    ) {
-        LazyColumn(
-            Modifier.padding(
-                start = searchLazyColumnPadding,
-                end = searchLazyColumnPadding
-            )
-        ) {
-            items(books) { book ->
-                BookRow(bookUi = book)
+
+    FadeVisibility(isVisible) {
+        if (!books.isNullOrEmpty()) {
+            LazyColumn(
+                Modifier.padding(
+                    start = searchLazyColumnPadding,
+                    end = searchLazyColumnPadding
+                )
+            ) {
+                items(books) { book ->
+                    BookRow(book, navController)
+                }
             }
         }
     }
+
 }
 
 @Composable
-fun ContainerView(uiState: UiState<List<BookUi>>) {
-    Row(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        if (uiState.isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(48.dp)
-            )
-        } else {
-            Text(
-                text = if (uiState.isEmpty) stringResource(R.string.no_books_found)
-                else uiState.message ?: "",
-                modifier = Modifier.padding(16.dp),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.body2
-            )
-        }
-    }
-}
-
-@Composable
-fun BookRow(bookUi: BookUi) {
+fun BookRow(
+    bookUi: BookUi,
+    navController: NavHostController
+) {
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(bottom = bookRowBottomPadding)
-            .clickable { },
+            .clickable {
+                val route = "${ReaderScreens.DetailScreen.name}/${bookUi.id}"
+                navController.navigate(route)
+            },
         shape = RectangleShape,
         backgroundColor = Color.White,
         elevation = bookRowElevation
