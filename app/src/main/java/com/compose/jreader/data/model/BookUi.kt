fa1e7874290b14507db39d36de8080904df19be5
Alin Babu <alinbabu2010@gmail.com>
2022-05-26 16:56:28 +0530

    Modify model class to be used for storing in firebase database and add extra parameters

--- app/src/main/java/com/compose/jreader/data/model/BookUi.kt
+++ app/src/main/java/com/compose/jreader/data/model/BookUi.kt
@@ -1,14 +1,51 @@
 package com.compose.jreader.data.model
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.PropertyName
+
 data class BookUi(
+
+    @Exclude
     var id: String = "",
+
     var title: String = "",
+
+    @get:PropertyName("book_photo_url")
+    @set:PropertyName("book_photo_url")
     var photoUrl: String = "",
+
     var authors: String = "",
+
     var description: String = "",
+
     var categories: String = "",
+
+    @get:PropertyName("published_date")
+    @set:PropertyName("published_date")
     var publishedDate: String = "",
-    var pageCount: Int = 0
+
+    @get:PropertyName("page_count")
+    @set:PropertyName("page_count")
+    var pageCount: Int = 0,
+    var rating: Double = 0.0,
+
+    @get:PropertyName("started_reading_at")
+    @set:PropertyName("started_reading_at")
+    var startedReading: Timestamp? = null,
+
+    @get:PropertyName("finished_reading_at")
+    @set:PropertyName("finished_reading_at")
+    var finishedReading: Timestamp? = null,
+
+    @get:PropertyName("user_id")
+    @set:PropertyName("user_id")
+    var userId: String = "",
+
+    @get:PropertyName("google_book_id")
+    @set:PropertyName("google_book_id")
+    var googleBookId: String = ""
+
 ) {
 
     companion object {
