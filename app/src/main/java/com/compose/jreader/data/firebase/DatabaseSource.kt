14c4a7a339fa39ffa2997a7600b28b96736fea5c
Alin Babu <alinbabu2010@gmail.com>
2022-05-27 16:42:30 +0530

    Add firebase database source

---
+++ app/src/main/java/com/compose/jreader/data/firebase/DatabaseSource.kt
@@ -0,0 +1,41 @@
+package com.compose.jreader.data.firebase
+
+import com.compose.jreader.data.model.BookUi
+import com.compose.jreader.utils.Constants
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+class DatabaseSource @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val fireStore: FirebaseFirestore
+) {
+
+    /**
+     * To save [BookUi] into [FirebaseFirestore]
+     * @param bookData [BookUi] model of the book to be saved
+     * @return CallBackFlow emitting a [Pair]<[Boolean],[String]>
+     * to denote task success and message in case of error
+     */
+    fun saveBookToFirebase(bookData: BookUi?) = callbackFlow {
+        bookData?.userId = firebaseAuth.currentUser?.uid ?: ""
+        val dbCollection = fireStore.collection(Constants.BOOK_DB_NAME)
+        if ((bookData != null) && bookData.toString().isNotBlank()) {
+            dbCollection.add(bookData).addOnSuccessListener { ref ->
+                dbCollection.document(ref.id).update(
+                    mapOf(Constants.USER_ID to ref.id)
+                ).addOnCompleteListener {
+                    if(it.isSuccessful) channel.trySend(null)
+                    else channel.trySend(it.exception?.localizedMessage)
+                }
+            }
+        }
+        awaitClose {
+            channel.close()
+        }
+    }
+
+
+}
