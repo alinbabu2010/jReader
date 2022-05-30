6b6e6364a2e3379da77340b5f6c918f4fd04f569
Alin Babu <alinbabu2010@gmail.com>
2022-05-30 11:40:00 +0530

    Add `homeViewModel` as parameter and load data to `BookListArea` based on `UiState`

--- app/src/main/java/com/compose/jreader/ui/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/compose/jreader/ui/screens/home/ReaderHomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,8 +25,10 @@ import androidx.navigation.NavHostController
 import com.compose.jreader.R
 import com.compose.jreader.data.model.BookUi
 import com.compose.jreader.ui.components.ListCard
+import com.compose.jreader.ui.components.LoaderMessageView
 import com.compose.jreader.ui.components.ReaderAppBar
 import com.compose.jreader.ui.components.TitleSection
+import com.compose.jreader.ui.model.UiState
 import com.compose.jreader.ui.navigation.ReaderScreens
 import com.compose.jreader.ui.screens.login.ReaderLoginViewModel
 import com.compose.jreader.ui.theme.Cyan200
@@ -34,7 +38,8 @@ import com.compose.jreader.utils.*
 @Composable
 fun ReaderHomeScreen(
     navController: NavHostController = NavHostController(LocalContext.current),
-    loginViewModel: ReaderLoginViewModel = hiltViewModel()
+    loginViewModel: ReaderLoginViewModel = hiltViewModel(),
+    homeViewModel: HomeViewModel = hiltViewModel()
 ) {
 
     Scaffold(topBar = {
@@ -56,7 +61,7 @@ fun ReaderHomeScreen(
                 .fillMaxWidth()
                 .verticalScroll(scrollState)
         ) {
-            HomeContent(navController, loginViewModel)
+            HomeContent(navController, loginViewModel, homeViewModel)
         }
 
     }
@@ -66,7 +71,8 @@ fun ReaderHomeScreen(
 @Composable
 fun HomeContent(
     navController: NavHostController,
-    loginViewModel: ReaderLoginViewModel
+    loginViewModel: ReaderLoginViewModel,
+    homeViewModel: HomeViewModel,
 ) {
 
     Column(
@@ -76,21 +82,25 @@ fun HomeContent(
         HomeTitleRow(Modifier.align(Alignment.Start), loginViewModel)
         ReadingRightNowArea(bookUis = BookUi.getBooks(), navController = navController)
         TitleSection(label = stringResource(R.string.reading_list))
-        BookListArea(bookUis = BookUi.getBooks(), navController = navController)
+        val uiState = homeViewModel.listOfBooks.collectAsState()
+        BookListArea(uiState, navController = navController)
     }
 
 }
 
 @Composable
 fun BookListArea(
-    bookUis: List<BookUi>,
+    state: State<UiState<List<BookUi>>>,
     navController: NavHostController
 ) {
-
-    HorizontalScrollContainer(bookUis = bookUis) {
-        Log.d("TAG", "BookListArea: $it")
+    val bookList = state.value.data
+    if (bookList == null) {
+        LoaderMessageView(state.value, "")
+    } else {
+        HorizontalScrollContainer(bookUis = bookList) {
+            Log.d("TAG", "BookListArea: $it")
+        }
     }
-
 }
 
 @Composable
