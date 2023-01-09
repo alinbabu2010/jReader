7332c852290e5ba674a3b354f1d11f9e9d397962
Alin Babu <alinbabu2010@gmail.com>
2023-01-09 19:00:43 +0530

    Add DI binding function for repository and databaseSource

--- app/src/main/java/com/compose/jreader/di/InterfaceModule.kt
+++ app/src/main/java/com/compose/jreader/di/InterfaceModule.kt
@@ -1,9 +1,12 @@
 package com.compose.jreader.di
 
+import com.compose.jreader.data.firebase.DatabaseSource
+import com.compose.jreader.data.firebase.DatabaseSourceImpl
 import com.compose.jreader.data.model.Book
 import com.compose.jreader.data.model.BookUi
 import com.compose.jreader.data.network.ApiLoader
 import com.compose.jreader.data.network.ApiLoaderImpl
+import com.compose.jreader.data.repository.*
 import com.compose.jreader.utils.BookToBookUiMapper
 import com.compose.jreader.utils.Mapper
 import dagger.Binds
@@ -24,5 +27,25 @@ abstract class InterfaceModule {
     abstract fun bindBookUiMapper(
         bookUiMapper: BookToBookUiMapper
     ): Mapper<Book, BookUi>
+
+    @Binds
+    abstract fun bindBookRepository(
+        bookRepositoryImpl: BookRepositoryImpl
+    ): BookRepository
+
+    @Binds
+    abstract fun bindFireStoreRepository(
+        fireStoreRepositoryImpl: FireStoreRepositoryImpl
+    ): FireStoreRepository
+
+    @Binds
+    abstract fun bindLoginRepository(
+        loginRepositoryImpl: LoginRepositoryImpl
+    ): LoginRepository
+
+    @Binds
+    abstract fun bindDatabaseSource(
+        databaseSourceImpl: DatabaseSourceImpl
+    ): DatabaseSource
 
 }
