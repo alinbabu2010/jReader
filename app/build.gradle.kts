b5de8ade64faef8569d90669ae34d652589e3947
Alin Babu <alinbabu2010@gmail.com>
2023-05-18 05:03:03 +0000

    Bump build number [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ val keystoreProperties = Properties().apply {
 
 android {
 
-    val buildNumber by extra { 6 }
+    val buildNumber by extra { 7 }
 
     applicationVariants.all {
         val variant = this
